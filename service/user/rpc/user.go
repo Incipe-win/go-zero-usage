62a744485b531f09784dd8da4f3ea1746b6629a4
hchao <huachaowu@qq.com>
2025-07-13 15:34:50 +0800

    update code

--- service/user/rpc/user.go
+++ service/user/rpc/user.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 
@@ -13,7 +14,10 @@ import (
 	"github.com/zeromicro/go-zero/core/service"
 	"github.com/zeromicro/go-zero/zrpc"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
 
 var configFile = flag.String("f", "etc/user.yaml", "the config file")
@@ -34,6 +38,22 @@ func main() {
 	})
 	defer s.Stop()
 
+	// 注册服务端拦截器
+	s.AddUnaryInterceptors(myInterceptor)
+
 	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
 	s.Start()
+}
+
+func myInterceptor(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp any, err error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, status.Errorf(codes.InvalidArgument, "missing metadata")
+	}
+	fmt.Printf("Received metadata: %v", md)
+	if md["token"][0] != "mall-order-h" {
+		return nil, status.Errorf(codes.Unauthenticated, "invalid token")
+	}
+	m, err := handler(ctx, req)
+	return m, err
 }
